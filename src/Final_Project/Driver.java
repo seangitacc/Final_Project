8bb1bc629806bb74cb4f6fde56cca364ec062529
seangitacc <seanmailacc@gmail.com>
2017-11-16 12:44:11 -0500

    First commit.

--- src/Final_Project/Driver.java
+++ src/Final_Project/Driver.java
@@ -1,4 +1,38 @@
 package Final_Project;
 
+import java.sql.*;
+
 public class Driver {
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        // Load the JDBC driver
+        Class.forName("com.mysql.jdbc.Driver");
+        System.out.println("Driver loaded");
+
+        String user = "root";
+        String passwd = "gr8isc00l";
+
+        // Establish a connection
+        Connection connection = DriverManager.getConnection
+                ("jdbc:mysql://35.196.66.63" + "/final-project?"
+                        + "user=" + user + "&password=" + passwd + "&autoReconnect=true&useSSL=false");
+        System.out.println("Database connected");
+
+        // Create a statement
+        Statement statement = connection.createStatement();
+
+        // Execute a statement
+        ResultSet resultSet = statement.executeQuery
+                ("select * from flights");
+
+        // Iterate through the result and print the student names
+        while (resultSet.next())
+            System.out.println(resultSet.getString(1) + "\t" + resultSet.getString(2));
+
+        // Close the connection
+        connection.close();
+
+
+    }
+
 }
