1bb80475eff065f52d0cc17574e3954e0f39e208
Nikhil Palli <npalli1@student.gsu.edu>
2017-12-03 13:16:52 -0500

    new GUI

--- src/Final_Project/Driver.java
+++ src/Final_Project/Driver.java
@@ -12,23 +12,6 @@ public class Driver {
 
 
 
-        JWindow window = new JWindow();
-        window.getContentPane().add(
-                new JLabel("", new ImageIcon(new URL ("http://bestanimations.com/Transport/Aircraft/Jumbo-08-june.gif")), SwingConstants.CENTER));
-        window.setBounds(450, 150, 400, 250);
-        window.setVisible(true);
-        try {
-            Thread.sleep(1000);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        window.dispose();
-
-        LoginScreen loginScreen = new LoginScreen();
-        loginScreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        loginScreen.setBounds(400, 150, 400, 200);
-        loginScreen.setSize(500,250);
-        loginScreen.setVisible(true);
 
         // Load the JDBC driver
         Class.forName("com.mysql.jdbc.Driver");
