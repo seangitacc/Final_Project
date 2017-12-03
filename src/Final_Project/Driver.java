d15abca7e67821dfb4fc3a7a3d998c89ec66297f
seangitacc <seanmailacc@gmail.com>
2017-12-03 13:08:00 -0500

    Removed splash screen

--- src/Final_Project/Driver.java
+++ src/Final_Project/Driver.java
@@ -10,20 +10,6 @@ public class Driver {
 
     public static void main(String[] args) throws Exception, ClassNotFoundException {
 
-
-
-        JWindow window = new JWindow();
-        window.getContentPane().add(
-                new JLabel("", new ImageIcon(new URL ("https://s.hswstatic.com/gif/airplanes-work-1.jpg")), SwingConstants.CENTER));
-        window.setBounds(500, 150, 300, 200);
-        window.setVisible(true);
-        try {
-            Thread.sleep(500);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        window.dispose();
-
         LoginScreen loginScreen = new LoginScreen();
         loginScreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         loginScreen.setBounds(5,5,300,200);
