fee5ee10ea476adb8c4eca254ee3f786a59f3558
seangitacc <seanmailacc@gmail.com>
2017-12-03 13:25:08 -0500

    Formatting and class changes

--- src/Final_Project/Driver.java
+++ src/Final_Project/Driver.java
@@ -1,17 +1,18 @@
 package Final_Project;
+
 import GUI.*;
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
 
-import javax.swing.*;
-import java.awt.*;
-import java.net.*;
-import java.sql.*;
-
-public class Driver {
+public class Driver extends Application {
 
     public static void main(String[] args) throws Exception, ClassNotFoundException {
-
-
-//a
 
         // Load the JDBC driver
         Class.forName("com.mysql.jdbc.Driver");
@@ -20,8 +21,44 @@ public class Driver {
         Utilities.createConnnection();;
         Utilities.createStatement();
 
+        launch ( args );
+
         // Close the connection
-        //Utilities.connection.close();
+        Utilities.connection.close();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+
+        Stage EntryWindow = new Stage ();
+        EntryWindow.setTitle ( "Ayrlyne" );
+        EntryWindow.setMinHeight ( 250 );
+
+        javafx.scene.control.Label welcomeMessage = new javafx.scene.control.Label( "Welcome to Ayrlyne!" );
+        welcomeMessage.setFont(new Font("Helvetica", 36));
+        javafx.scene.control.Button loginButton = new javafx.scene.control.Button("Click to Login!");
+        javafx.scene.control.Button registerButton = new Button("Click to Register!");
+
+        loginButton.setOnAction ( e -> LoginWindow.display () );
+        registerButton.setOnAction ( e -> RegisterWindow.dispaly () );
+        VBox vb = new VBox(20);
+        HBox hb = new HBox(20);
+
+        Scene entryScene;
+
+        hb.getChildren ().addAll (loginButton,registerButton );
+        vb.getChildren ().addAll ( welcomeMessage,hb );
+        vb.setAlignment ( Pos.CENTER );
+        hb.setAlignment ( Pos.CENTER );
+
+
+        entryScene = new Scene ( vb , 1250  ,  700);
+
+
+        EntryWindow.setScene ( entryScene );
+        primaryStage = EntryWindow;
+        primaryStage.show();
 
     }
 
