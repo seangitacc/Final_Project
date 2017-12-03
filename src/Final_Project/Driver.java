908cdf1f6a59f4e190ad0557e9b832562d854c16
Nikhil Palli <npalli1@student.gsu.edu>
2017-12-03 13:15:29 -0500

    new GUI

--- src/Final_Project/Driver.java
+++ src/Final_Project/Driver.java
@@ -1,18 +1,32 @@
 package Final_Project;
 import GUI.*;
-import com.sun.javafx.scene.control.skin.DatePickerSkin;
 
 import javax.swing.*;
-import java.net.URL;
+import java.awt.*;
+import java.net.*;
 import java.sql.*;
 
 public class Driver {
 
     public static void main(String[] args) throws Exception, ClassNotFoundException {
 
+
+
+        JWindow window = new JWindow();
+        window.getContentPane().add(
+                new JLabel("", new ImageIcon(new URL ("http://bestanimations.com/Transport/Aircraft/Jumbo-08-june.gif")), SwingConstants.CENTER));
+        window.setBounds(450, 150, 400, 250);
+        window.setVisible(true);
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        window.dispose();
+
         LoginScreen loginScreen = new LoginScreen();
         loginScreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        loginScreen.setBounds(5,5,300,200);
+        loginScreen.setBounds(400, 150, 400, 200);
         loginScreen.setSize(500,250);
         loginScreen.setVisible(true);
 
